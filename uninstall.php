2f20794cd9163433641b52eb5140786b28696ff1
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-07 02:02:42 +0300

    Empty file

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Unistall Container Block.
+ * Fired when the plugin is uninstalled.
+ *
+ * @author      Mahdi Yazdani
+ * @package     Block Data Attribute
+ * @since       1.0.0
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+} // End If Statement
