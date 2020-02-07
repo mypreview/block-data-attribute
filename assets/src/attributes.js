3b4017af0f693799e7fbe1ad8ce7264693c4af6a
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-07 15:10:25 +0300

    Use "lodash" instead of native assign method

--- assets/src/attributes.js
+++ assets/src/attributes.js
@@ -3,37 +3,35 @@
  */
 import includes from 'lodash/includes';
 import allowedBlocks from './allowedBlocks';
+require( 'es6-object-assign/auto' );
 
 /**
  * WordPress dependencies
  */
 const { addFilter } = wp.hooks;
-const { hasBlockSupport } = wp.blocks;
 
 /**
  * Filters registered block settings, extending attributes with anchor using ID
  * of the first node.
  *
- * @param 	{Object}   settings    Original block settings.
- * @return 	{Object} 			   Filtered block settings.
+ * @param 	{Object}   blockSettings    Original block settings.
+ * @return 	{Object} 			   		Filtered block settings.
  */
-function addAttributes( settings ) {
-	if ( 'undefined' !== typeof settings.attributes ) {
-		if ( allowedBlocks.includes( settings.name ) ) {
-			settings.attributes = Object.assign( settings.attributes, {
-				bdaLimit: {
-					type: 'number',
-					default: 1
-				},
-				bdaData: {
-			        type: 'array',
-			        default: []
-			    }
-			} );
-		}
-	}
+function addAttributes( blockSettings ) {
+	if ( 'undefined' === typeof blockSettings.attributes || ! allowedBlocks.includes( blockSettings.name ) ) return blockSettings;
 
-	return settings;
+	return lodash.assign( {}, blockSettings, {
+        attributes: lodash.assign( {}, blockSettings.attributes, {
+            bdaLimit: {
+				type: 'number',
+				default: 1
+			},
+			bdaData: {
+		        type: 'array',
+		        default: []
+		    }
+        } )
+    } );
 }
 
 addFilter(
