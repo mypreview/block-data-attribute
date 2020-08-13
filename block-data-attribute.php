8c5227df26d93477b2ba8507c51f195942512e6e
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-08-14 01:39:28 +0300

    Bumped plugin version
    Code tidy

--- block-data-attribute.php
+++ block-data-attribute.php
@@ -22,9 +22,9 @@
  * Plugin Name:             Block Data Attribute
  * Plugin URI:              https://www.mypreview.one
  * Description:             This plugin designed with extensibility in mind for data that should be associated with a particular block type but need not have any defined meaning.
- * Version:                 1.0.3
+ * Version:                 1.0.4
  * Author:                  MyPreview
- * Author URI:              https://www.upwork.com/o/profiles/users/_~016ad17ad3fc5cce94
+ * Author URI:              https://mahdiyazdani.com
  * License:                 GPL-3.0
  * License URI:             http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:             block-data-attribute
@@ -81,13 +81,11 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 		 * @return  instance
 		 */
 		public static function instance() {
-
 			if ( is_null( self::$instance ) ) {
 				self::$instance = new self();
 			} // End If Statement
 
 			return self::$instance;
-
 		}
 
 		/**
@@ -97,11 +95,9 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 		 * @return  void
 		 */
 		protected function __construct() {
-
 			add_action( 'init', array( $this, 'textdomain' ) );
 			add_action( 'enqueue_block_editor_assets', array( $this, 'editor_enqueue' ) );
 			add_filter( sprintf( 'plugin_action_links_%s', BLOCK_DATA_ATTRIBUTE_PLUGIN_BASENAME ), array( $this, 'additional_links' ) );
-
 		}
 
 		/**
@@ -111,9 +107,7 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 		 * @return  void
 		 */
 		protected function __clone() {
-
 			_doing_it_wrong( __FUNCTION__, esc_html_x( 'Cloning instances of this class is forbidden.', 'clone', 'block-data-attribute' ), esc_html( BLOCK_DATA_ATTRIBUTE_VERSION ) );
-
 		}
 
 		/**
@@ -123,9 +117,7 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 		 * @return  void
 		 */
 		public function __wakeup() {
-
 			_doing_it_wrong( __FUNCTION__, esc_html_x( 'Unserializing instances of this class is forbidden.', 'wakeup', 'block-data-attribute' ), esc_html( BLOCK_DATA_ATTRIBUTE_VERSION ) );
-
 		}
 
 		/**
@@ -136,9 +128,7 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 		 * @return  void
 		 */
 		public function textdomain() {
-
 			load_plugin_textdomain( 'block-data-attribute', false, dirname( dirname( BLOCK_DATA_ATTRIBUTE_PLUGIN_BASENAME ) ) . '/languages/' );
-
 		}
 
 		/**
@@ -148,7 +138,6 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 		 * @return   void
 		 */
 		public function editor_enqueue() {
-
 			$script_path       = sprintf( '%sdist/script.js', BLOCK_DATA_ATTRIBUTE_DIR_PATH );
 			$script_asset_path = sprintf( '%sdist/script.asset.php', BLOCK_DATA_ATTRIBUTE_DIR_PATH );
 			$script_asset      = file_exists( $script_asset_path ) ? require $script_asset_path : array(
@@ -159,7 +148,6 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 			// Enqueue the script.
 			wp_enqueue_script( BLOCK_DATA_ATTRIBUTE_SLUG, $script_url, $script_asset['dependencies'], $script_asset['version'], true );
 			wp_set_script_translations( BLOCK_DATA_ATTRIBUTE_SLUG, 'block-data-attribute', sprintf( '%s/languages/', BLOCK_DATA_ATTRIBUTE_DIR_PATH ) );
-
 		}
 
 		/**
@@ -171,7 +159,6 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 		 * @return  array   $links
 		 */
 		public function additional_links( $links ) {
-
 			$plugin_links = array();
 			/* translators: 1: Open anchor tag, 2: Close anchor tag. */
 			$plugin_links[] = sprintf( _x( '%1$sHire Me!%2$s', 'plugin link', 'block-data-attribute' ), sprintf( '<a href="%s" class="button-link-delete" target="_blank" rel="noopener noreferrer nofollow" title="%s">', esc_url( BLOCK_DATA_ATTRIBUTE_AUTHOR_URI ), esc_attr_x( 'Looking for help? Hire Me!', 'upsell', 'block-data-attribute' ) ), '</a>' );
@@ -179,7 +166,6 @@ if ( ! class_exists( 'Block_Data_Attribute' ) ) :
 			$plugin_links[] = sprintf( _x( '%1$sSupport%2$s', 'plugin link', 'block-data-attribute' ), sprintf( '<a href="https://wordpress.org/support/plugin/%s" target="_blank" rel="noopener noreferrer nofollow">', BLOCK_DATA_ATTRIBUTE_SLUG ), '</a>' );
 
 			return array_merge( $plugin_links, $links );
-
 		}
 
 	}
