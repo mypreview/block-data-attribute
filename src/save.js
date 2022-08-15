a929fb35f0bef2c04ffcc91bcd30d2da6bfec6a4
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-15 17:28:33 +0300

    Allow not `data` prefixed attributes to be created

--- src/save.js
+++ src/save.js
@@ -1,40 +1,31 @@
 /**
  * External dependencies
  */
-import { get, times, has, includes, escape, filter, assign } from 'lodash';
-import allowedBlocks from './allowedBlocks';
+import { normalizeJsonify } from '@mypreview/unicorn-js-utils';
+import { assign, map, isUndefined } from 'lodash';
 
 /**
  * WordPress dependencies
  */
-const { addFilter } = wp.hooks;
+import { addFilter } from '@wordpress/hooks';
 
 /**
  * A filter that applies to the block returning a WP Element in the save function.
  * This filter is used to add extra props to the root element of the save function.
  *
- * @param  	{Object} 	extraProps 		Additional props applied to save element.
- * @param  	{Object} 	blockType  		Block type.
- * @param  	{Object} 	attributes 		Current block attributes.
- * @return 	{Object}            		Filtered props applied to save element.
+ * @param  	  {Object}    extraProps    Additional props applied to save element.
+ * @param  	  {Object}    blockType     Block type.
+ * @param  	  {Object}    attributes    Current block attributes.
+ * @return    {Object}               	Filtered props applied to save element.
  */
 function addSaveProps( extraProps, blockType, attributes ) {
-	const extraAttrs = {};
-	const { bdaLimit: limit, bdaData: data } = attributes;
+	const { blockDataAttribute } = attributes;
 
-	if ( includes( allowedBlocks, blockType.name ) ) {
-		const filterData = filter( data, ( o ) => ( has( o, 'value' ) ? o.value : null ) );
-		{
-			times( limit, ( index ) => {
-				const key = get( filterData, [ index, 'key' ] ),
-					value = get( filterData, [ index, 'value' ] );
+	// Bail early, in case block has no border attribute associated with it.
+	if ( isUndefined( blockDataAttribute ) ) {
+		return extraProps;
+	}
 
-				if ( !! key && !! value ) extraAttrs[ `data-${ escape( key ) }` ] = escape( value );
-			} );
-		}
-	} // End If Statement
-
-	return assign( extraProps, extraAttrs );
+	return assign( {}, extraProps, ...map( normalizeJsonify( blockDataAttribute ), ( { name: attrName, value } ) => ( { [ attrName ]: value } ) ) );
 }
-
-addFilter( 'blocks.getSaveContent.extraProps', 'block-data-attribute/save', addSaveProps );
+addFilter( 'blocks.getSaveContent.extraProps', 'mypreview/block-data-attribute/save', addSaveProps );
