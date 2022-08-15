0ee0b1f6e9591d53fc40d2e34c077396fa7ec195
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-15 17:27:44 +0300

    Defined list of allowed blocks

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ */
+export default Object.freeze( {
+	ALLOWED_BLOCKS: [ 'core/button', 'core/column', 'core/columns', 'core/heading', 'core/group', 'core/paragraph' ],
+} );
